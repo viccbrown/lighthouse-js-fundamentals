89fd0e994cc841fc12165394c0734ac470940e20
Victoria Brown <vbrown1344@gmail.com>
2021-10-15 16:09:33 -0400

    isEven function

---
+++ even.js
@@ -0,0 +1,22 @@
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+// following example is a simplified version of the above code
+
+const isEven  = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEven(10));
+console.log(isEven(11));
